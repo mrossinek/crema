d6f929ee29e7a1cf80e35c4c29a2ebb580c2d221
Max Rossmannek <max.rossmannek@uzh.ch>
2021-01-08 21:53:43 +0100

    WIP: Configuration rehaul
    This is the first step to changing CoBib's configuration to Python.
    Based on [this Stackoverlow answer][1], the new `Config` is a subclass
    of Python's `dict`. Besides the usual field access, the elements can
    also be set and accessed via attributes.
    
    Note: as this is a WIP and standlone commit, no unittest is likely to
    pass for this specific commit.
    
    [1]: https://stackoverflow.com/a/3031270

--- cobib/config.py
+++ cobib/config.py
@@ -1,7 +1,6 @@
 """CoBib configuration module."""
 
-from copy import deepcopy
-import configparser
+import importlib.util
 import io
 import logging
 import os
@@ -20,158 +19,62 @@ ANSI_COLORS = [
     'white',
 ]
 
-DEFAULTS = {
-    'DATABASE': {
-        'file': os.path.expanduser('~/.local/share/cobib/literature.yaml'),
-        'git': False,
-        'open': 'xdg-open' if sys.platform.lower() == 'linux' else 'open',
-        'grep': 'grep',
-        'search_ignore_case': False,
-    },
-    'FORMAT': {
-        'month': 'int',
-        'ignore_non_standard_types': False,
-        'default_entry_type': 'article',
-    },
-    'TUI': {
-        'default_list_args': '-l',
-        'prompt_before_quit': True,
-        'reverse_order': True,
-        'scroll_offset': 3,
-    },
-    'KEY_BINDINGS': {
-    },
-    'COLORS': {
-        'cursor_line_fg': 'white',
-        'cursor_line_bg': 'cyan',
-        'top_statusbar_fg': 'black',
-        'top_statusbar_bg': 'yellow',
-        'bottom_statusbar_fg': 'black',
-        'bottom_statusbar_bg': 'yellow',
-        'search_label_fg': 'blue',
-        'search_label_bg': 'black',
-        'search_query_fg': 'red',
-        'search_query_bg': 'black',
-        'popup_help_fg': 'white',
-        'popup_help_bg': 'green',
-        'popup_stdout_fg': 'white',
-        'popup_stdout_bg': 'blue',
-        'popup_stderr_fg': 'white',
-        'popup_stderr_bg': 'red',
-        'selection_fg': 'white',
-        'selection_bg': 'magenta',
-    },
-}
-
-XDG_CONFIG_FILE = '~/.config/cobib/config.ini'
+XDG_CONFIG_FILE = '~/.config/cobib/config.py'
 
 
-class Config:
-    """Class used solely for the global configuration object."""
+class Config(dict):
+    """CoBib's configuration object.
 
-    def __init__(self):
-        """Initializes the configuration data dictionary."""
-        self.config = {}
+    Source: https://stackoverflow.com/a/3031270
+    """
 
-    def set_config(self, configpath=None):
-        """Sets the configuration.
+    MARKER = object()
 
-        If a configuration file is provided as a keyword argument it is used instead of the default
-        paths. The configparser module is used to parse the INI configuration file.
+    # pylint: disable=super-init-not-called
+    def __init__(self, value=None):
+        if value is None:
+            pass
+        elif isinstance(value, dict):
+            for key in value:
+                self.__setitem__(key, value[key])
+        else:
+            raise TypeError('expected dict')
 
-        Args:
-            configpath (str or io.TextIOWrapper, optional): the path to an optional configuration
-                                                            file.
-        """
-        ini_conf = configparser.ConfigParser()
-        ini_conf.optionxform = str  # makes option names case-sensitive!
-        # load default configuration
-        ini_conf.read_dict(DEFAULTS)
+    def __setitem__(self, key, value):
+        if isinstance(value, dict) and not isinstance(value, Config):
+            value = Config(value)
+        super().__setitem__(key, value)
 
-        # read ini config file
+    def __getitem__(self, key):
+        found = self.get(key, Config.MARKER)
+        if found is Config.MARKER:
+            found = Config()
+            super().__setitem__(key, found)
+        return found
+
+    __setattr__, __getattr__ = __setitem__, __getitem__
+
+    @staticmethod
+    def load(configpath=None):
+        """TODO"""
         if configpath is not None:
             if isinstance(configpath, io.TextIOWrapper):
                 configpath = configpath.name
             LOGGER.info('Loading configuration from %s', configpath)
-            ini_conf.read(configpath)
         elif os.path.exists(os.path.expanduser(XDG_CONFIG_FILE)):
             LOGGER.info('Loading configuration from default location: %s',
                         os.path.expanduser(XDG_CONFIG_FILE))
-            ini_conf.read(os.path.expanduser(XDG_CONFIG_FILE))
-
-        # overwrite settings
-        for section in ini_conf.sections():
-            self.config[section] = deepcopy(ini_conf[section])
+            configpath = os.path.expanduser(XDG_CONFIG_FILE)
+        else:
+            return
+        spec = importlib.util.spec_from_file_location("config", configpath)
+        cfg = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(cfg)
 
     def validate(self):
         """Validates the configuration at runtime."""
         LOGGER.info('Validating the runtime configuration.')
-
-        # DATABASE section
-        LOGGER.debug('Validing the DATABASE configuration section.')
-        self._assert(self.config.get('DATABASE', None) is not None,
-                     "Missing DATABASE section.")
-        self._assert(isinstance(self.config.get('DATABASE', {}).get('file', None), str),
-                     "DATABASE/file should be a string.")
-        self._assert(isinstance(self.config.get('DATABASE', {}).getboolean('git', None), bool),
-                     "DATABASE/git should be a boolean.")
-        self._assert(isinstance(self.config.get('DATABASE', {}).get('open', None), str),
-                     "DATABASE/open should be a string.")
-        self._assert(isinstance(self.config.get('DATABASE', {}).get('grep', None), str),
-                     "DATABASE/grep should be a string.")
-        self._assert(isinstance(
-            self.config.get('DATABASE', {}).getboolean('search_ignore_case', None), bool),
-                     "DATABASE/search_ignore_case should be a boolean.")
-
-        # FORMAT section
-        LOGGER.debug('Validing the FORMAT configuration section.')
-        self._assert(self.config.get('FORMAT', None) is not None,
-                     "Missing FORMAT section.")
-        self._assert(self.config.get('FORMAT', {}).get('month', None) in ('int', 'str'),
-                     "FORMAT/month should be either 'int' or 'str'.")
-        self._assert(isinstance(
-            self.config.get('FORMAT', {}).getboolean('ignore_non_standard_types', None), bool),
-                     "FORMAT/ignore_non_standard_types should be a boolean.")
-        self._assert(isinstance(self.config.get('FORMAT', {}).get('default_entry_type', None), str),
-                     "FORMAT/default_entry_type should be a string.")
-
-        # TUI section
-        LOGGER.debug('Validing the TUI configuration section.')
-        self._assert(self.config.get('TUI', None) is not None,
-                     "Missing TUI section.")
-        self._assert(isinstance(self.config.get('TUI', {}).get('default_list_args', None), str),
-                     "TUI/default_list_args should be a string.")
-        self._assert(isinstance(
-            self.config.get('TUI', {}).getboolean('prompt_before_quit', None), bool),
-                     "TUI/prompt_before_quit should be a boolean.")
-        self._assert(isinstance(
-            self.config.get('TUI', {}).getboolean('reverse_order', None), bool),
-                     "TUI/reverse_order should be a boolean.")
-        self._assert(isinstance(
-            self.config.get('TUI', {}).getint('scroll_offset', None), int),
-                     "TUI/scroll_offset should be an integer.")
-
-        # KEY_BINDINGS section
-        LOGGER.debug('Validing the KEY_BINDINGS configuration section.')
-        self._assert(self.config.get('KEY_BINDINGS', None) is not None,
-                     "Missing KEY_BINDINGS section.")
-        # actual key bindings are asserted when mapped by the TUI instance
-
-        # COLORS section
-        LOGGER.debug('Validing the COLORS configuration section.')
-        self._assert(self.config.get('COLORS', None) is not None,
-                     "Missing COLORS section.")
-        for name in DEFAULTS['COLORS']:
-            self._assert(name in self.config.get('COLORS', {}).keys(),
-                         f"Missing value for COLORS/{name}")
-        available_colors = ('black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white')
-        for name, color in self.config.get('COLORS', {}).items():
-            if name not in DEFAULTS['COLORS'] and name not in available_colors:
-                LOGGER.warning('Ignoring unknown TUI color: %s', name)
-            self._assert(color in available_colors or
-                         (len(color.strip('#')) == 6 and
-                          tuple(int(color.strip('#')[i:i+2], 16) for i in (0, 2, 4))),
-                         f"Unknown color specification: {color}")
+        # TODO
 
     @staticmethod
     def _assert(expression, error):
@@ -193,10 +96,65 @@ class Config:
         Returns:
             A string representing the foreground and background ANSI color code.
         """
-        fg_color = 30 + ANSI_COLORS.index(self.config['COLORS'].get(name + '_fg'))
-        bg_color = 40 + ANSI_COLORS.index(self.config['COLORS'].get(name + '_bg'))
+        fg_color = 30 + ANSI_COLORS.index(self.tui.colors.get(name + '_fg'))
+        bg_color = 40 + ANSI_COLORS.index(self.tui.colors.get(name + '_bg'))
 
         return f'\x1b[{fg_color};{bg_color}m'
 
+    def defaults(self):
+        """TODO"""
+        self.database.file = os.path.expanduser('~/.local/share/cobib/literature.yaml')
+        self.database.git = False
+        self.database.open = 'xdg-open' if sys.platform.lower() == 'linux' else 'open'
+        self.database.grep = 'grep'
+        self.database.search_ignore_case = False
 
-CONFIG = Config()
+        self.format.month = int
+        self.format.ignore_non_standard_types = False
+        self.format.default_entry_type = 'article'
+
+        self.tui.default_list_args = '-l'
+        self.tui.prompt_before_quit = True
+        self.tui.reverse_order = True
+        self.tui.scroll_offset = 3
+
+        self.tui.colors.cursor_line_fg = 'white'
+        self.tui.colors.cursor_line_bg = 'cyan'
+        self.tui.colors.top_statusbar_fg = 'black'
+        self.tui.colors.top_statusbar_bg = 'yellow'
+        self.tui.colors.bottom_statusbar_fg = 'black'
+        self.tui.colors.bottom_statusbar_bg = 'yellow'
+        self.tui.colors.search_label_fg = 'blue'
+        self.tui.colors.search_label_bg = 'black'
+        self.tui.colors.search_query_fg = 'red'
+        self.tui.colors.search_query_bg = 'black'
+        self.tui.colors.popup_help_fg = 'white'
+        self.tui.colors.popup_help_bg = 'green'
+        self.tui.colors.popup_stdout_fg = 'white'
+        self.tui.colors.popup_stdout_bg = 'blue'
+        self.tui.colors.popup_stderr_fg = 'white'
+        self.tui.colors.popup_stderr_bg = 'red'
+        self.tui.colors.selection_fg = 'white'
+        self.tui.colors.selection_bg = 'magenta'
+
+        self.tui.key_bindings.prompt = ':'
+        self.tui.key_bindings.search = '/'
+        self.tui.key_bindings.help = '?'
+        self.tui.key_bindings.add = 'a'
+        self.tui.key_bindings.delete = 'd'
+        self.tui.key_bindings.edit = 'e'
+        self.tui.key_bindings.filter = 'f'
+        self.tui.key_bindings.modify = 'm'
+        self.tui.key_bindings.open = 'o'
+        self.tui.key_bindings.quit = 'q'
+        self.tui.key_bindings.redo = 'r'
+        self.tui.key_bindings.sort = 's'
+        self.tui.key_bindings.undo = 'u'
+        self.tui.key_bindings.select = 'v'
+        self.tui.key_bindings.wrap = 'w'
+        self.tui.key_bindings.export = 'x'
+        self.tui.key_bindings.show = 'ENTER'
+
+
+config = Config()
+config.defaults()
